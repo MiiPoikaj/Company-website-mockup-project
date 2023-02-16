c4b01818448b0fec795ba95c873f268906974323
Miika Poikajärvi <miika.poikajarvi@gmail.com>
2023-02-16 21:16:44 +0200

    Added first nav

--- react-app/little-lemon/src/components/Nav.js
+++ react-app/little-lemon/src/components/Nav.js
@@ -1,13 +1,15 @@
-
+import React from "react";
 function Nav () {
-    <nav>
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="404.html">Menu</a></li> 
-            <li><a href="404.html">Book</a></li>
-            <li><a href="404.html">About</a></li>
-        </ul>
-    </nav>
+    return (
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="404.html">Menu</a></li> 
+                <li><a href="404.html">Book</a></li>
+                <li><a href="404.html">About</a></li>
+            </ul>
+        </nav>
+    )
 };
 
 export default Nav;
