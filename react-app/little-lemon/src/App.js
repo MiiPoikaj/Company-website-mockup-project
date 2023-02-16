5acae424cb272fde64a7ea6a5ffc2ab244fbaadb
Miika Poikajärvi <miika.poikajarvi@gmail.com>
2023-02-16 21:37:57 +0200

    Added footer

--- react-app/little-lemon/src/App.js
+++ react-app/little-lemon/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Promotion from './components/Promotion';
 import Card from './components/Card';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Nav />
       <Promotion />
       <Card />
+      <Footer />
     </div>
   );
 }
