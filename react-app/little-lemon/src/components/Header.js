bdb9b916eaa905572a3875a9a285ac9086041234
Miika Poikajärvi <miika.poikajarvi@gmail.com>
2023-02-16 21:03:29 +0200

    Added initial components

---
+++ react-app/little-lemon/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './Resources/Images/Asset 16@4x.png'
+
+function Header () {
+    return (
+        <header>
+            <img>{logo}</img>
+        </header>
+    )
+};
+
+export default Header;
---
+++ react-app/little-lemon/src/components/Nav.js
@@ -0,0 +1,13 @@
+
+function Nav () {
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="404.html">Menu</a></li> 
+            <li><a href="404.html">Book</a></li>
+            <li><a href="404.html">About</a></li>
+        </ul>
+    </nav>
+};
+
+export default Nav;
